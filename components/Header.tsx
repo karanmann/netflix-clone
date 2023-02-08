25f4dee4ecbbfcb366ced0abd704081123121944
Karan Mann <karan.mann@outlook.com>
2023-02-08 15:46:27 +0100

    dev: Header component with basic styling and Icons

---
+++ components/Header.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { BellIcon, SearchIcon } from '@heroicons/react/solid'
+import Link from 'next/link';
+const Header = () => {
+  return (
+    <header>
+      <div className="flex items-center space-x-2 md:space-x-10">
+        <img
+          src="https://rb.gy/ulxxee"
+          width={100}
+          height={100}
+          alt="netflix"
+          className="object-contain cursor-pointer"
+        />
+        <ul className="hidden space-x-4 md:flex">
+          <li className="headerLink">Home</li>
+          <li className="headerLink">TV Shows</li>
+          <li className="headerLink">Movies</li>
+          <li className="headerLink">New & Popular</li>
+          <li className="headerLink">My List</li>
+        </ul>
+      </div>
+      <div className="flex items-center space-x-4 text-sm font-light">
+        <SearchIcon className="hidden w-6 h-6 sm:inline" />
+        <p className="hidden lg:inline">Kids</p>
+        <BellIcon className="w-6 h-6" />
+        <Link href="/account">
+          <img
+            src="https://rb.gy/g1pwyx"
+            alt="user"
+            className="rounded cursor-pointer"
+          />
+        </Link>
+      </div>
+    </header>
+  );
+}
+
+export default Header
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,25 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Header from "../components/Header";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>Build Netflix</div>
-      
+      <Header />
+      <main>
+        {/* BANNER */}
+        <section>
+          {/* CONTENT */}
+        </section>
+      </main>
+      {/* MODAL */}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
