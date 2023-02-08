05c9c1dd1dd1ca0fc9cdb1d5a841c8a103445522
Karan Mann <karan.mann@outlook.com>
2023-02-08 17:15:40 +0100

    dev: All data types imported and added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,29 @@
-
 import Head from "next/head";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
 import requests from "../utils/requests";
+import { Movie } from "../typings";
+interface Props {
+  netflixOriginals: Movie[];
+  trendingNow: Movie[];
+  topRated: Movie[];
+  actionMovies: Movie[];
+  comedyMovies: Movie[];
+  horrorMovies: Movie[];
+  romanceMovies: Movie[];
+  documentaries: Movie[];
+}
 
-const Home: React.FC = ({ netflixOriginals }) => {
-  console.log(netflixOriginals)
+const Home = ({
+  netflixOriginals,
+  actionMovies,
+  comedyMovies,
+  documentaries,
+  horrorMovies,
+  romanceMovies,
+  topRated,
+  trendingNow
+}: Props) => {
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
