e3a24dfeea8d58505efc669a4086355d8118a197
Karan Mann <karan.mann@outlook.com>
2023-02-09 11:33:24 +0100

    dev: Text shadow added to content for more visibility

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -30,19 +30,19 @@ const Banner = ({netflixOriginals}: Props) => {
           />
         )}
       </div>
-      <h1 className="text-2xl lg:text-7xl md:text-4xl ">
+      <h1 className="text-2xl lg:text-7xl md:text-4xl">
         {movie?.title || movie?.name || movie?.original_name}
       </h1>
-      <p className="max-w-xs text-xs md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">
+      <p className="max-w-xs text-xs md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl text-shadow-md">
         {movie?.overview}
       </p>
       <div className="flex space-x-3">
         <button className="text-black bg-white bannerButton">
-          <FaPlay className="w-4 h-4 text-black md:h-7 md:w-7"/> 
+          <FaPlay className="w-4 h-4 text-black md:h-7 md:w-7" />
           Play
         </button>
         <button className="bannerButton bg-[gray]/70">
-          <InformationCircleIcon className="w-5 h-5 md:h-8 md:w-8"/>
+          <InformationCircleIcon className="w-5 h-5 md:h-8 md:w-8" />
           More Info
         </button>
       </div>
