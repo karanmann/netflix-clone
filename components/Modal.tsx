0d092110e103bc9cdeb295f3c321b3193b1c907e
karanmann <karan.mann@outlook.com>
2023-02-15 16:13:40 +0100

    dev: Mute Unmute functionality and some more interface buttons added

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,16 +1,27 @@
 import React, { useEffect, useState } from "react";
 import MuiModal from "@mui/material/Modal";
+import ReactPlayer from "react-player/lazy";
 import { useRecoilState } from "recoil";
 import { modalState, movieState } from "../atoms/modalAtoms";
-import { XIcon } from "@heroicons/react/outline";
+import {
+  CheckIcon,
+  PlusIcon,
+  ThumbUpIcon,
+  VolumeOffIcon,
+  VolumeUpIcon,
+  XIcon,
+} from "@heroicons/react/outline";
 import { Genre, Element } from "../typings";
 import { element } from "prop-types";
+import { FaPlay } from "react-icons/fa";
 
 const Modal = () => {
   const [showModal, setShowModal] = useRecoilState(modalState);
   const [movie, setMovie] = useRecoilState(movieState);
   const [trailer, setTrailer] = useState();
   const [genres, setGenres] = useState<Genre[]>([]);
+  const [muted, setMuted] = useState(false);
+  const [addedToList, setAddedToList] = useState(false)
 
   useEffect(() => {
     if (!movie) return;
@@ -45,9 +56,12 @@ const Modal = () => {
     setShowModal(false);
   };
 
-  console.log(trailer);
   return (
-    <MuiModal open={showModal} onClick={handleClose}>
+    <MuiModal
+      open={showModal}
+      onClose={handleClose}
+      className="fixed !top-7 left-0 right-0 z-50 mx-auto w-full max-w-5xl overflow-hidden overflow-y-scroll rounded-md scrollbar-hide"
+    >
       <>
         <button
           onClick={handleClose}
@@ -55,6 +69,41 @@ const Modal = () => {
         >
           <XIcon className="h-6 w-6" />
         </button>
+        <div className="relative pt-[56.25%]">
+          <ReactPlayer
+            url={`https://www.youtube.com/watch?v=${trailer}`}
+            width="100%"
+            height="100%"
+            style={{ position: "absolute", top: "0", left: "0" }}
+            playing
+            muted={muted}
+          />
+          <div className="absolute bottom-10 flex w-full items-center justify-between px-10">
+            <div className="flex space-x-2">
+              <button className="flex h-11 items-center gap-x-2 rounded bg-white px-5 text-xl font-bold text-black transition hover:bg-[#e6e6e6]">
+                <FaPlay className="h-7 w-7 text-black" />
+                Play
+              </button>
+              <button className="modalButton">
+                <ThumbUpIcon className="h-6 w-6" />
+              </button>
+              <button className="modalButton">
+                {addedToList ? (
+                  <CheckIcon className="h-7 w-7" />
+                ) : (
+                  <PlusIcon className="h-7 w-7" />
+                )}
+              </button>
+            </div>
+            <button className="modalButton" onClick={() => setMuted(!muted)}>
+                {muted ? (
+                  <VolumeOffIcon className="h-6 w-6" />
+                ) : (
+                  <VolumeUpIcon className="h-6 w-6" />
+                )}
+              </button>
+          </div>
+        </div>
       </>
     </MuiModal>
   );
