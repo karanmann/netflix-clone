02ab3833875fce9cd9983ed70abb9b97b577d06b
karanmann <karan.mann@outlook.com>
2023-02-15 10:14:01 +0100

    dev: updated env+gitignore - Login Input Created

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,7 +28,7 @@ const Home = ({
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
-        <title>Create Next App</title>
+        <title>Netflix - Home</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
